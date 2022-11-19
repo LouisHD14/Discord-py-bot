91a7c7029bb097d7b70279b5ff3d0e2412d87d39
LouisHD14 <114616518+LouisHD14@users.noreply.github.com>
2022-11-19 23:27:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import discord
 
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print ("Booting up your system")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+    
+bot.run("MTAzODQ2MDQxODk0OTEyNDIxOA.GiAjxe.MPWrPf1MUDvrecl-HoLn9dbc7RWUDpfZUOAVno")
